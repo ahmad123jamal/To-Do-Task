1e801b0e775dfca4ea9bd2ef32cd5bf462eb964f
ahmad123jamal <you@example.com>
2024-09-22 12:50:07 +0500

    html css and javascript file are created and code are added

---
+++ script.js
@@ -0,0 +1,5 @@
+const addTask = document.getElementById('addTask');
+const button = document.getElementById('btn');
+const showTask = document.getElementById('showTask');
+console.log(addTask,btn,showTask)
+
