520da99a725c4e8011d34f889f9ad93f17694da5
ahmad123jamal <you@example.com>
2024-09-28 00:41:27 +0500

    some styling are added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const textInput = document.getElementById('textInput');
+let textInput = document.getElementById('textInput');
 const addTask = document.getElementById('addTask');
 const showTask = document.getElementById('showTask');
 
@@ -7,13 +7,13 @@ const showTask = document.getElementById('showTask');
 
 
 addTask.addEventListener('click', () => {
-// create condition for empty value
+
   if (!textInput.value.trim()) {
    alert(' Please enter your task ')
-   return; // Stop further execution if input is empty
+   return; 
   }
 
-  // create an li element, create span elemen add classes
+  
   const li = document.createElement('li');
   const span = document.createElement("span");
   span.textContent = textInput.value;
@@ -23,16 +23,16 @@ addTask.addEventListener('click', () => {
   
 
  
-  // add & remove linethrough
+  
     span.addEventListener("click",function(){
       span.classList.toggle("liToggle")
     })
  
 
-// Create remove button
+
   const removeBtn = document.createElement("button")
   removeBtn.textContent = "Remove";
-  // apply design class for remove button
+ 
   removeBtn.classList.add('removeBtnDesign');
   removeBtn.addEventListener('click', () => {
   showTask.removeChild(li);
