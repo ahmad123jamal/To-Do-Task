244565cbdd74d9d3c25e0c2d897a70f83d8d3191
ahmad123jamal <you@example.com>
2024-09-28 00:23:27 +0500

    code modified

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
+
+const textInput = document.getElementById('textInput');
 const addTask = document.getElementById('addTask');
-const button = document.getElementById('btn');
 const showTask = document.getElementById('showTask');
 
 
-var li = document.createElement('li');
-
-li.textContent = addTask.value;
-
-var rmvbtn = document.createElement('button')
 
 
 addTask.addEventListener('click', () => {
-    showTask = addTask.value
-})
+// create condition for empty value
+  if (!textInput.value.trim()) {
+   alert(' Please enter your task ')
+   return; // Stop further execution if input is empty
+  }
+
+  // create an li element, create span elemen add classes
+  const li = document.createElement('li');
+  const span = document.createElement("span");
+  span.textContent = textInput.value;
+  li.classList.add('liElement');
+  span.classList.add('hoverEffect')
+
+  
+
+ 
+  // add & remove linethrough
+    span.addEventListener("click",function(){
+      span.classList.toggle("liToggle")
+    })
+ 
+
+// Create remove button
+  const removeBtn = document.createElement("button")
+  removeBtn.textContent = "Remove";
+  // apply design class for remove button
+  removeBtn.classList.add('removeBtnDesign');
+  removeBtn.addEventListener('click', () => {
+  showTask.removeChild(li);
+  });
+  
 
 
-
-
-
-
-
-
-if (addTask.Value.trim()) {
-    alert("plz enter your task")
-}
-
-
-
-
-
-
-
+  
+  li.append(span)
+  li.append(removeBtn);
+  showTask.append(li);
+  textInput.value = '';
+});
 
